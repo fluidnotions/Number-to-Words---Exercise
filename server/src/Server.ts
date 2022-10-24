855da9eb1a2bc80e8a734cfce96e7d81abbce2c2
Justin Robinson <justin.g.robinson@gmail.com>
2022-10-24 17:48:54 +0200

    single start cmd complete

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -3,22 +3,17 @@ import cors from 'cors'
 import { NumberToWords } from './NumberToWords';
 
 const app: Express = express();
-app.use(cors)
+app.use(cors())
 const port = 4000;
 
 app.post('/number-to-words/:number', (req: Request, res: Response) => {
     const { params: { number } } = req
     console.log(`app.get -> number`, number)
-    try {
-      const result = NumberToWords.convert(parseInt(number))
-      console.log(`app.get -> result`, result)
-      res.send({
-          result
-      })
-    } catch (error) {
-      console.error(error)
-    }
-    
+    const result = NumberToWords.convert(parseInt(number))
+    console.log(`app.get -> result`, result)
+    res.send({
+        result
+    })
 });
 
 app.listen(port, () => {
